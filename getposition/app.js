533ec639d9caa352f1a0672efe45f53023904f25
rajatmw1999 <rajatis1999@gmail.com>
2020-01-14 15:26:04 +0530

    getposition and watchposition

---
+++ getposition/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+
+
+app.listen(3000 || process.env.PORT, function(){
+  console.log('Server is running');
+});
